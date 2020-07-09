092e7071c7be38bcb30212cda4ef2ea6069beb53
Marcotte173 <47604041+Marcotte173@users.noreply.github.com>
2020-07-09 16:45:30 -0600

    Add files via upload
    First Upload

---
+++ GameJam2020Change/GameJam2020Change/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GameJam2020Change
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hey Everyone. Ready to make a game?");
+        }
+    }
+}
