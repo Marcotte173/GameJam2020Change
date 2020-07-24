e86f2056c71b7499cbd6ae3baa519d1b408d424d
SpawnsRight <40549738+SpawnsRight@users.noreply.github.com>
2020-07-24 18:28:42 -0400

    Update zorkStart.cs

--- Vacillator/Vacillator/Zork/zorkStart.cs
+++ Vacillator/Vacillator/Zork/zorkStart.cs
@@ -3,24 +3,25 @@ using System.Collections.Generic;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading;
-//public class zorkStart : Room
-//{
- //   public zorkStart()
- //   {
- //       //Sends you to Good ending track
- //       if (placeholder == placeholder) 
- //       { 
- //       
- //       }
- //       //Sends you to Neutral ending track
- //       else if (placeholder == placeholder)
- //       {
- //
- //       }
- //       //Sends you to Bad ending track
- //       else
- //       {
- //
- //       }
- //   }
- //}
+public class zorkStart : Room
+{
+   public zorkStart()
+   {
+       //Sends you to Good ending track
+       if (Shock.good && Caged.good && Loot.good) 
+       { 
+          zorkGoodEnd();
+       }
+       //Sends you to Neutral ending track
+       else if (Shock.good || Caged.good || Loot.good)
+       {
+         zorkNeutralEnd();
+       }
+       //Sends you to Bad ending track
+       else
+       {
+         zorkBadEnd();
+
+       }
+   }
+}
